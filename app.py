dfc1eb945461f58aa39cb0fae3640bc1cf07c20b
stuby67 <ssgaikwad284@gmail.com>
2024-10-06 15:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,30 @@ app = FastAPI()
 # Mount the static files correctly
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
+# Home route
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/start", response_class=HTMLResponse)
-async def start_game(request: Request):
+# Route for Game Selection page
+@app.get("/game-selection", response_class=HTMLResponse)
+async def game_selection(request: Request):
+    return templates.TemplateResponse("game_selection.html", {"request": request})
+
+# Routes for each game
+@app.get("/game1", response_class=HTMLResponse)
+async def game1(request: Request):
     return templates.TemplateResponse("game1.html", {"request": request})
+
+@app.get("/game2", response_class=HTMLResponse)
+async def game2(request: Request):
+    return templates.TemplateResponse("game2.html", {"request": request})
+
+@app.get("/game3", response_class=HTMLResponse)
+async def game3(request: Request):
+    return templates.TemplateResponse("game3.html", {"request": request})
 
 # Run the app using: uvicorn app:app --reload
